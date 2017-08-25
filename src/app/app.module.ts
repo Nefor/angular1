137cfcb673303ba8bee718abc9b0d24da2e3d908
Volodymyr Leshchenko <v.v.grimm@gmail.com>
2017-08-25 21:40:03 +0300

    inventory app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { UserItemComponent } from './user-item/user-item.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { ArticleComponent } from './article/article.component';
+import { InventoryAppComponent } from './inventory-app/inventory-app.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +21,13 @@ import { ArticleComponent } from './article/article.component';
     HelloWorldComponent,
     UserItemComponent,
     UserListComponent,
-    ArticleComponent
+    ArticleComponent,
+    InventoryAppComponent,
+    ProductsListComponent,
+    ProductRowComponent,
+    ProductImageComponent,
+    ProductDepartmentComponent,
+    PriceDisplayComponent
   ],
   imports: [
     BrowserModule,
